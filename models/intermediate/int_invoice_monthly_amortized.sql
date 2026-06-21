8211555f2b8ce4b573321168029b7d096b24e8f3
Elkadev <el.kakagianni@gmail.com>
2026-06-22 00:09:19 +0300

    fix: correct amortization rounding and months_covered calculation

--- models/intermediate/int_invoice_monthly_amortized.sql
+++ models/intermediate/int_invoice_monthly_amortized.sql
@@ -17,7 +17,7 @@ with invoices as (
         , i.billing_end_date
         , i.amount_usd
         , p.billing_frequency
-        , datediff('month', i.billing_start_date, i.billing_end_date) as months_covered
+        , datediff('month', i.billing_start_date, i.billing_end_date) + 1 as months_covered
     from invoices as i
     inner join products as p
         on i.product_id = p.product_id
@@ -38,13 +38,37 @@ with invoices as (
         , unnest(
             generate_series(
                 cast(date_trunc('month', billing_start_date) as date)
-                , cast(date_trunc('month', billing_end_date) - interval '1 day' as date)
+                , cast(date_trunc('month', billing_end_date) as date)
                 , interval '1 month'
             )
         )::date as month
-        , round(amount_usd / months_covered, 2) as mrr_usd
+        , round(amount_usd / months_covered, 2) as mrr_usd_base
     from invoices_enriched
-    where months_covered > 0
+    where months_covered >= 1
 )
 
-select * from amortized
+, with_remainder as (
+    select
+        *
+        , row_number() over (partition by invoice_id order by month desc) as rn
+        , amount_usd - (round(amount_usd / months_covered, 2) * months_covered) as remainder
+    from amortized
+)
+
+select
+    invoice_id
+    , customer_id
+    , subscription_id
+    , product_id
+    , invoice_date
+    , billing_start_date
+    , billing_end_date
+    , amount_usd
+    , billing_frequency
+    , months_covered
+    , month
+    , case
+        when rn = 1 then mrr_usd_base + remainder
+        else mrr_usd_base
+      end as mrr_usd
+from with_remainder
---
+++ query.py
@@ -0,0 +1,7 @@
+﻿import duckdb
+import sys
+
+con = duckdb.connect('mrr_analytics.duckdb', read_only=True)
+sql = " ".join(sys.argv[1:]) if len(sys.argv) > 1 else "SHOW TABLES"
+print(con.sql(sql).df().to_string())
+con.close()
